87666977af7ce0d6ed6419a0adb212166d6ea43e
zh-lx <18366276315@163.com>
2023-02-28 09:14:54 +0800

    feat: 增加 webpack5 Vue3 的测试

--- packages/webpack-plugin/src/index.ts
+++ packages/webpack-plugin/src/index.ts
@@ -1,26 +1,32 @@
-import HtmlWebpackPlugin from 'html-webpack-plugin';
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 import { getInjectCode, startServer } from 'vue-inspector-core';
 const path = require('path');
 class TrackCodePlugin {
-  apply(complier) {
+  apply(compiler) {
     // 仅在开发环境下使用
-    if (complier.options.mode === 'development') {
-      complier.hooks.watchRun.tap('TrackCodePlugin', () => {
-        complier.options.module.rules.push({
-          test: /\.vue$/,
-          use: [path.resolve(__dirname, './loader.js')],
-          enforce: 'pre',
+    if (compiler.options.mode === 'development') {
+      if (compiler.hooks) {
+        compiler.hooks.watchRun.tap('VueInspectorLoader', () => {
+          compiler.options.module.rules.push({
+            test: /\.vue$/,
+            use: [path.resolve(__dirname, './loader.js')],
+            enforce: 'pre',
+          });
         });
-      });
 
-      complier.hooks.compilation.tap('TrackCodePlugin', (compilation) => {
-        startServer((port) => {
-          const code = getInjectCode(port);
-          // html-webpack-plugin 4 之前版本
-          if (compilation.hooks.htmlWebpackPluginAfterHtmlProcessing) {
-            compilation.hooks.htmlWebpackPluginAfterHtmlProcessing.tapAsync(
-              'TrackCodePlugin',
-              (data, cb) => {
+        compiler.hooks.compilation.tap(
+          'WebpackVueInspectorPlugin',
+          (compilation) => {
+            startServer((port) => {
+              const code = getInjectCode(port);
+              // HtmlWebpackPlugin3 及之前版本
+              let hook = compilation.hooks.htmlWebpackPluginAfterHtmlProcessing;
+              if (!hook) {
+                // 4 及之后版本
+                hook = HtmlWebpackPlugin.getHooks(compilation).beforeEmit;
+              }
+              console.log(hook);
+              hook.tapAsync('VueInspectorPlugin', (data, cb) => {
                 const index = data.html.lastIndexOf('</html>');
                 if (index > -1) {
                   const newHTML =
@@ -30,27 +36,11 @@ class TrackCodePlugin {
                   data.html = newHTML;
                   cb(null, data);
                 }
-              }
-            );
-          } else {
-            // 适应 html-webpack-plugin  5 版本
-            HtmlWebpackPlugin?.getHooks?.(compilation)?.beforeEmit?.tapAsync(
-              'TrackCodePlugin',
-              (data, cb) => {
-                const index = data.html.lastIndexOf('</html>');
-                if (index > -1) {
-                  const newHTML =
-                    data.html.slice(0, index) +
-                    `\n${code}\n` +
-                    data.html.slice(index);
-                  data.html = newHTML;
-                  cb(null, data);
-                }
-              }
-            );
+              });
+            });
           }
-        });
-      });
+        );
+      }
     }
   }
 }
--- packages/webpack-test/vue.config.js
+++ packages/webpack-test/vue.config.js
@@ -3,9 +3,7 @@ const DebugPlugin = require('webpack-vue-inspector-plugin');
 module.exports = {
   // ...other code
   chainWebpack: (config) => {
-    if (process.env.NODE_ENV === 'development') {
-      // add this configuration in the development environment
-      config.plugin('webpack-vue-inspector-plugin').use(new DebugPlugin());
-    }
+    // add this configuration in the development environment
+    config.plugin('webpack-vue-inspector-plugin').use(new DebugPlugin());
   },
 };
