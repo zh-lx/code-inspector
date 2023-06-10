b7cf60c4dbd216cbc7859ca919a8fed1f136d8ef
zh-lx <18366276315@163.com>
2023-06-10 09:35:20 +0800

    fix: 修复 vite 无法识别环境变量的问题

--- packages/core/src/server/launch-editor.ts
+++ packages/core/src/server/launch-editor.ts
@@ -5,6 +5,7 @@ const child_process = require('child_process');
 const os = require('os');
 const chalk = require('chalk');
 const shellQuote = require('shell-quote');
+const dotenv = require('dotenv');
 
 function isTerminalEditor(editor: string) {
   switch (editor) {
@@ -182,17 +183,19 @@ function getArgumentsForLineNumber(
   return [fileName];
 }
 
-/**
- * webpack 会自动注入 `WEBPACK_DEV_SERVER` 
- * 
- * 需要根据是否为 vite 环境来获取不同的环境变量值
- */
-const isVite = !process.env.WEBPACK_DEV_SERVER
-
 function guessEditor() {
-  // Explicit config always wins
-  if (isVite ? process.env.VITE_CODE_EDITOR : process.env.CODE_EDITOR) {
-    return shellQuote.parse(isVite ? process.env.VITE_CODE_EDITOR : process.env.CODE_EDITOR);
+  // webpack
+  if (process.env.CODE_EDITOR) {
+    return shellQuote.parse(process.env.CODE_EDITOR);
+  }
+
+  // vite
+  const envPath = path.resolve(process.cwd(), '.env.local');
+  const envFile = fs.readFileSync(envPath, 'utf-8');
+  const envConfig = dotenv.parse(envFile || '');
+  if (envConfig.CODE_EDITOR) {
+    console.log(shellQuote.parse(envConfig.CODE_EDITOR));
+    return shellQuote.parse(envConfig.CODE_EDITOR);
   }
 
   // We can find out which editor is currently running by:
@@ -270,7 +273,7 @@ function printInstructions(fileName: any, errorMessage: string | any[] | null) {
   }
   console.log(
     'To set up the editor integration, add something like ' +
-      chalk.cyan(`${isVite ? 'VITE_CODE_EDITOR=atom' : 'CODE_EDITOR=atom'}`) +
+      chalk.cyan('CODE_EDITOR=atom') +
       ' to the ' +
       chalk.green('.env.local') +
       ' file in your project folder ' +
