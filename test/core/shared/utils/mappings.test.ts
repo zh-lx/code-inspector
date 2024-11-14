69eec6682b83586e416e6f04d936e03e28160158
zhoulixiang <zhoulixiang@kuaishou.com>
2024-11-14 08:53:46 +0800

    test: update test for mapppings

--- test/core/shared/utils/mappings.test.ts
+++ test/core/shared/utils/mappings.test.ts
@@ -1,39 +1,55 @@
 import { getMappingFilePath } from '@/core/src/shared/utils';
-import { expect, describe, it } from 'vitest';
+import { expect, describe, it, vi, beforeEach } from 'vitest';
+import fs from 'fs';
 
 describe('mappings', () => {
   const file = '/Users/zlx/code-inspector/node_modules/pkg-1/a/b/c';
   const correntPath = '/Users/zlx/code-inspector/packages/pkg-1/a/b/c';
 
-  it('record: short find no slash, replacement no slash', () => {
+  beforeEach(() => {
+    vi.mock('fs');
+    vi.mocked(fs.existsSync).mockReturnValue(true);
+  });
+
+  it('return real file with record and slash', () => {
     const mappings = {
       'pkg-1': '/Users/zlx/code-inspector/packages/pkg-1',
-    }
-    const target = getMappingFilePath(file, mappings)
+    };
+    const target = getMappingFilePath(file, mappings);
     expect(target).to.be.equal(correntPath);
+  });
+
+  it('return original file when real file not found', () => {
+    vi.mocked(fs.existsSync).mockReturnValue(false);
+    const mappings = {
+      'pkg-1': '/Users/zlx/code-inspector/packages/pkg-1',
+    };
+    const target = getMappingFilePath(file, mappings);
+    expect(target).to.be.equal(file);
   });
 
   it('record: short find slash, replacement slash', () => {
     const mappings = {
       'pkg-1/': '/Users/zlx/code-inspector/packages/pkg-1/',
-    }
-    const target = getMappingFilePath(file, mappings)
+    };
+    const target = getMappingFilePath(file, mappings);
     expect(target).to.be.equal(correntPath);
   });
 
   it('record: full find', () => {
     const mappings = {
-      '/Users/zlx/code-inspector/node_modules/pkg-1': '/Users/zlx/code-inspector/packages/pkg-1/',
-    }
-    const target = getMappingFilePath(file, mappings)
+      '/Users/zlx/code-inspector/node_modules/pkg-1':
+        '/Users/zlx/code-inspector/packages/pkg-1/',
+    };
+    const target = getMappingFilePath(file, mappings);
     expect(target).to.be.equal(correntPath);
   });
 
   it('record: no match', () => {
     const mappings = {
       'pkg-2': '/Users/zlx/code-inspector/packages/pkg-2',
-    }
-    const target = getMappingFilePath(file, mappings)
+    };
+    const target = getMappingFilePath(file, mappings);
     expect(target).to.be.equal(file);
   });
 
@@ -46,9 +62,9 @@ describe('mappings', () => {
       {
         find: 'pkg-1',
         replacement: '/Users/zlx/code-inspector/packages/pkg-1',
-      }
-    ]
-    const target = getMappingFilePath(file, mappings)
+      },
+    ];
+    const target = getMappingFilePath(file, mappings);
     expect(target).to.be.equal(correntPath);
   });
 
@@ -61,9 +77,9 @@ describe('mappings', () => {
       {
         find: /pkg\-1/,
         replacement: '/Users/zlx/code-inspector/packages/pkg-1',
-      }
-    ]
-    const target = getMappingFilePath(file, mappings)
+      },
+    ];
+    const target = getMappingFilePath(file, mappings);
     expect(target).to.be.equal(correntPath);
   });
 
@@ -76,15 +92,15 @@ describe('mappings', () => {
       {
         find: /pkg\-3/,
         replacement: '/Users/zlx/code-inspector/packages/pkg-1',
-      }
-    ]
-    const target = getMappingFilePath(file, mappings)
+      },
+    ];
+    const target = getMappingFilePath(file, mappings);
     expect(target).to.be.equal(file);
   });
 
   it('empty mappings', () => {
     const mappings = undefined;
-    const target = getMappingFilePath(file, mappings)
+    const target = getMappingFilePath(file, mappings);
     expect(target).to.be.equal(file);
   });
 });
