65bdb73b5875df368d531e3ea3372eb83cecbc92
zhoulixiang <zhoulixiang@kuaishou.com>
2024-12-15 23:56:11 +0800

    perf:optimize filepath from absolute to relative

--- packages/core/types/server/server.d.ts
+++ packages/core/types/server/server.d.ts
@@ -1,3 +1,5 @@
-import type { CodeOptions, RecordInfo } from '../shared';
+import { type CodeOptions, type RecordInfo } from '../shared';
+export declare const ProjectRootPath: string;
+export declare function getShortagePath(filePath: string): string;
 export declare function createServer(callback: (port: number) => any, options?: CodeOptions): void;
 export declare function startServer(options: CodeOptions, record: RecordInfo): Promise<void>;
