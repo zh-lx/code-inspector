a0c18356b0403c6410794a6197aa2d2e062c33d3
zhoulixiang <zhoulixiang@kuaishou.com>
2025-02-22 13:09:02 +0800

    fix: compatible for pug syntax in vue with webpack

--- demos/webpack5-vue3/vue.config.js
+++ demos/webpack5-vue3/vue.config.js
@@ -9,5 +9,11 @@ module.exports = {
         bundler: 'webpack',
       })
     );
+
+    config.module
+      .rule('pug')
+      .test(/\.pug$/) // 替换为你的文件扩展名
+      .use('pug-plain-loader')
+      .loader('pug-plain-loader') // 替换为你的 loader 名称
   },
 };
