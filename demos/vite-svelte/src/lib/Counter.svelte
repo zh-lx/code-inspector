6c5f4dcb79652b308b5f6f2ebc873092fc5a9428
yaoyuanzhang <945947732@qq.com>
2025-04-11 22:25:37 +0800

    chore: fix svelte demo.

---
+++ demos/vite-svelte/src/lib/Counter.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    let count: number = 0;
+    const increment = () => {
+        count += 1;
+    };
+</script>
+
+<button on:click={increment}>
+    count is {count}
+</button>
+
+<h1>test</h1>
