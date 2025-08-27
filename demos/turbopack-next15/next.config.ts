8e1f65fe44ae247b85020236022c333e5f62203d
17hz <0x149527@gmail.com>
2025-08-27 11:05:17 +0800

    feat: create MDX demo page

--- demos/turbopack-next15/next.config.ts
+++ demos/turbopack-next15/next.config.ts
@@ -1,7 +1,13 @@
 import { codeInspectorPlugin } from 'code-inspector-plugin';
 import type { NextConfig } from 'next';
+import createMDX from '@next/mdx'
+
+const withMDX = createMDX({
+  // Add markdown plugins here, as desired
+})
 
 const nextConfig: NextConfig = {
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
   turbopack: {
     rules: codeInspectorPlugin({
       bundler: 'turbopack',
@@ -9,4 +15,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+export default withMDX(nextConfig)
